b3be388a0cbeb989a1df3cf2a57030d1dbe256cb
dinelka <newlakshan3@gmail.com>
2024-11-26 11:55:17 +0530

    add logs FieldController

--- src/main/java/lk/ijse/cropMonitoringSystem/api/FieldController.java
+++ src/main/java/lk/ijse/cropMonitoringSystem/api/FieldController.java
@@ -4,6 +4,8 @@ import lk.ijse.cropMonitoringSystem.DTO.FieldDTO;
 import lk.ijse.cropMonitoringSystem.DTO.StaffDTO;
 import lk.ijse.cropMonitoringSystem.exception.DataPersistException;
 import lk.ijse.cropMonitoringSystem.service.FieldService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,36 +21,48 @@ import java.util.Map;
 @CrossOrigin(origins = "http://localhost:63342")
 @RequestMapping("api/v1/fields")
 public class FieldController {
+    static Logger logger= LoggerFactory.getLogger(FieldController.class);
+
     @Autowired
     private FieldService fieldService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, String>> saveField(@RequestBody FieldDTO fieldDTO) {
+        logger.info("saveField method call");
         Map<String, String> response = new HashMap<>();
         if (!fieldDTO.getFieldCode().matches("^FLD-00\\d+$")) {
+            logger.info("check validation ");
             response.put("error", "Invalid field code. Expected format: FLD-00");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+
         }
+
         try {
             fieldService.saveField(fieldDTO);
             response.put("message", "Field and staff data saved successfully");
+            logger.info("Field saved ");
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (DataPersistException e) {
             e.printStackTrace();
+            logger.info("Field not saved bad request ");
             response.put("error", "Bad request: Invalid data");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         } catch (Exception e) {
             e.printStackTrace();
+            logger.info("Field Internal server error");
             response.put("error", "Internal server error");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Map<String, Object>>> getAllFields() {
+        logger.info("getAllFields called");
         try {
             List<Map<String, Object>> fieldsWithStaffIds = fieldService.getAllFields();
+            logger.info("get All Field");
             return ResponseEntity.ok(fieldsWithStaffIds);
         } catch (Exception e) {
+            logger.info("error getAll Field");
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ArrayList<>());
         }
@@ -56,15 +70,19 @@ public class FieldController {
 
     @GetMapping("/{fieldCode}")
     public ResponseEntity<FieldDTO> getFieldAndStaff(@PathVariable String fieldCode) {
+        logger.info("getFieldAndStaff method called ");
         // Validate the fieldCode format
         if (!fieldCode.matches("^FLD-\\d{3}$")) {
+            logger.info("getFieldAndStaff validation sucessfully");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
         }
         FieldDTO fieldDTO = fieldService.getSelectedStaffAndField(fieldCode);
         return new ResponseEntity<>(fieldDTO, HttpStatus.OK);
     }
     @GetMapping("/details/{fieldCode}")
     public ResponseEntity<FieldDTO> getOnlyField(@PathVariable String fieldCode) {
+        logger.info("getOnlyField Method called");
         FieldDTO fieldDetails = fieldService.getOnlySelectedField(fieldCode);
         return new ResponseEntity<>(fieldDetails, HttpStatus.OK);
     }
@@ -75,12 +93,11 @@ public class FieldController {
     }
     @PutMapping("/{fieldCode}")
     public ResponseEntity<FieldDTO> updateFieldAndStaff(@PathVariable String fieldCode, @RequestBody FieldDTO fieldDTO) {
-        // Validate the fieldCode format
-        if (!fieldCode.matches("^FLD-\\d{3}$")) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+        logger.info("updateFieldAndStaff Method called");
+
         try {
             FieldDTO updatedField = fieldService.updateFieldAndStaff(fieldCode, fieldDTO);
+            logger.info("updated sucess");
             return new ResponseEntity<>(updatedField, HttpStatus.OK);
         } catch (RuntimeException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -91,12 +108,15 @@ public class FieldController {
 
     @DeleteMapping("/{fieldCode}")
     public ResponseEntity<Void> deleteFieldAndStaff(@PathVariable String fieldCode) {
+        logger.info("deleteFieldAndStaff Method called");
         // Validate the fieldCode format
         if (!fieldCode.matches("^FLD-\\d{3}$")) {
+            logger.info("delete Field Id validation ok");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         try {
             fieldService.deleteFieldAndStaff(fieldCode);
+            logger.info("delete Field SucessFully");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (RuntimeException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
