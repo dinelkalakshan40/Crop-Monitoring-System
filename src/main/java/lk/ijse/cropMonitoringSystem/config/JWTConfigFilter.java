0affc00723d4c2d4f09219e6fcca7dae7f41315d
dinelka <newlakshan3@gmail.com>
2024-12-01 16:33:47 +0530

    Add JWTConfig

--- src/main/java/lk/ijse/cropMonitoringSystem/config/JWTConfigFilter.java
+++ src/main/java/lk/ijse/cropMonitoringSystem/config/JWTConfigFilter.java
@@ -8,6 +8,10 @@ import lk.ijse.cropMonitoringSystem.service.AuthService.JWTService;
 import lk.ijse.cropMonitoringSystem.service.AuthService.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,7 +19,7 @@ import java.io.IOException;
 @Configuration
 @RequiredArgsConstructor
 
-public class JWTConfigFilter extends OncePerRequestFilter { //Ensures filter executed once per request
+public class JWTConfigFilter extends OncePerRequestFilter { //ALL request filter executed once per request
     private final JWTService jwtService;
     private final UserService userService;
     @Override
@@ -30,6 +34,25 @@ public class JWTConfigFilter extends OncePerRequestFilter { //Ensures filter exe
             return;
         }
         extractedJwtToken = initToken.substring(7);
-        userEmail =jwtService.
+        userEmail =jwtService.extractUserName(extractedJwtToken);
+
+        // user email
+        if(org.apache.commons.lang3.StringUtils.isNotEmpty(userEmail) &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails =
+                    userService.userDetailsService().loadUserByUsername(userEmail);
+            if(jwtService.validateToken(extractedJwtToken, userDetails)) {
+                //add user to the security context
+                SecurityContext emptyContext =
+                        SecurityContextHolder.createEmptyContext();
+                var authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,null,userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                emptyContext.setAuthentication(authToken);
+                SecurityContextHolder.setContext(emptyContext);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
